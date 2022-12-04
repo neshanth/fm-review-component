1bf563b9a8f0cc1f07fe7f645f93d07a0764b8f4
neshanth <59483685+neshanth@users.noreply.github.com>
2022-12-04 09:52:27 +0530

    added form for rating buttons

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,8 @@ const button = document.querySelector(".btn");
 const ratingCard = document.querySelector(".rating-content");
 const thankYou = document.querySelector(".thank-you");
 const numbers = document.querySelectorAll(".rating-numbers span");
-const rating = document.querySelector(".rating");
+const rating = document.querySelector(".rating-final");
+const form = document.querySelector("form");
 let selectedRating = 5;
 
 const handleRating = (e) => {
@@ -15,11 +16,26 @@ const removeSelectedRatings = () => {
   numbers.forEach((number) => number.classList.remove("rating-selected-value"));
 };
 
-button.addEventListener("click", (e) => {
+// button.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   console.log(e);
+//   ratingCard.classList.add("hide-review");
+//   thankYou.classList.remove("hide-thank-you");
+//   rating.innerText = selectedRating;
+// });
+
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  ratingCard.classList.add("hide-review");
-  thankYou.classList.remove("hide-thank-you");
-  rating.innerText = selectedRating;
+  let checkedInput = document.querySelector('input[name="rating"]:checked');
+  if (checkedInput) {
+    selectedRating = checkedInput.value;
+    ratingCard.classList.add("hide-review");
+    thankYou.classList.remove("hide-thank-you");
+    rating.innerText = selectedRating;
+  } else {
+    alert("Please choose a rating");
+    return;
+  }
 });
 
 numbers.forEach((number) => number.addEventListener("click", handleRating));
